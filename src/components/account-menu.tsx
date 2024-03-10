3ea6c49c88d3762c0a7b12d104d128fafbcd1361
matheusmntt <matheus.monteiro7@outlook.com>
2024-03-10 01:42:38 -0300

    pizza shop

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,75 @@
+import { useQuery } from '@tanstack/react-query'
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
+
+export const AccountMenu = () => {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+    })
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          {isLoadingManagedRestaurant && <Skeleton className="h-4 w-40" />}
+          {!isLoadingManagedRestaurant && managedRestaurant?.name}
+          <ChevronDown className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          {isLoadingProfile && (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          )}
+          {!isLoadingProfile && (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className="cursor-pointer">
+          <Building className="mr-2 size-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="cursor-pointer text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 size-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,55 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetails } from './order-details'
+
+export const OrderTableRow = () => {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="size-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+
+      <TableCell className="font-mono text-xs font-medium">klçwqeklq</TableCell>
+
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="size-2 rounded-full bg-slate-400"></span>
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+
+      <TableCell className="font-medium">Matheus Monteiro</TableCell>
+
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 size-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 size-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
